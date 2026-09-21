a5670084b13de9c318718b1956c882dc281eed5b
MostafaSensei106 <wwwmostafagmer100'gmail.com>
2026-09-21 13:36:42 +0300

    Remove unused modules and files from the Flutter project, including core utilities, authentication, catalog, CRM, delivery, inventory, orders, and POS modules. This cleanup enhances project maintainability and reduces unnecessary dependencies.

--- Flutter/packages/core/network/lib/src/payloads.g.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'payloads.dart';
-
-// **************************************************************************
-// JsonSerializableGenerator
-// **************************************************************************
-
-EmptyDto _$EmptyDtoFromJson(Map<String, dynamic> json) => EmptyDto();
